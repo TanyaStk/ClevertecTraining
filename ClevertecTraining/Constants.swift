111f5cad682bb3d5d64183687bbdfe964ae2f48a
Tanya Samastroyenka <tanyasamastr@gmail.com>
2022-12-08 13:14:40 +0300

    MODIFIED constants

--- ClevertecTraining/Constants.swift
+++ ClevertecTraining/Constants.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 enum Constants {
-    static let heightMultiplier = 0.08
+    static let heightMultiplier = 0.06
     static let widthMultiplier = 0.9
     static let offset = 8
+    static let cornerRadius: CGFloat = 8
 }
